c37ad2bc0d20f52f18d94eb01ba5ea4e03504777
TJ <cooploops@gmail.com>
2018-06-21 23:40:55 -0700

    finish assignment

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  subscriptions = ['Basic','Advanced','Pro'];
+  defaultSub = 'Advanced';
+  @ViewChild('form') practiceForm:NgForm;
+  submitted = false;
+
+  data = {
+    email:'',
+    subscription:'',
+    password:''
+  }
+
+  onSubmit(){
+    console.log(this.practiceForm);
+
+    this.submitted = true;
+    this.data.email = this.practiceForm.value.email;
+    this.data.subscription = this.practiceForm.value.Subscriptions
+    this.data.password = this.practiceForm.value.password;
+    
+    this.practiceForm.reset();
+  }
+
+}
