601867f8b4cca7484c7c8a0ed870a894c37f2d3e
TJ <cooploops@gmail.com>
2018-07-12 23:07:17 -0700

    finish assignment

---
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any): any {
+    return value.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+}
