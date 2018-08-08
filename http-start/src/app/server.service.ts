d62530f21769a249e2d58a63bc8f9544e4aeb03d
TJ <cooploops@gmail.com>
2018-08-07 23:24:33 -0700

    start http module

---
+++ http-start/src/app/server.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class ServerService {
+    constructor(private http: Http) {
+
+    }
+
+    storeServers(servers: Array<any>) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+        // return this.http.post('https://udemy-ng-http-26adb.firebaseio.com/data.json', servers, {headers: headers});
+        return this.http.put('https://udemy-ng-http-26adb.firebaseio.com/data.json', servers, {headers: headers});
+    }
+    getServers() {
+        return this.http.get('https://udemy-ng-http-26adb.firebaseio.com/data.json')
+        .pipe(map(
+            (response: Response) => {
+                const data = response.json();
+                return data;
+            }
+        ));
+    }
+}
